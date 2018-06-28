e6d736fd052b870bdde88337a05e044cf3e760e9
Kane <52653784@qq.com>
2018-06-28 11:56:34 +0800

    Start

--- src/SDK/TWUsa.php
+++ src/SDK/TWUsa.php
@@ -1,27 +1,27 @@
 <?php
 namespace Peak\SDK;
 
-//use Peak\SDK\TWUsa\Core as SDK;
+use Peak\SDK\TWUsa\Core as SDK;
 
-class TWUsa extends \Peak\SDK\TWUsa\Core {
+class TWUsa {
 
 	private static $sdk;
 
 	/**
 	 * @param $auth array , key is the class name of authenticate method, val is certificate
 	 * */
-	/*
 	function __construct($apiKey, $apiSecret, $devMod=false)
 	{
 		self::$sdk = new SDK($apiKey, $apiSecret, $devMod);
 	}
 
 
-
-
+	public $result;
 	public function addSku ($param)
 	{
-		$res = self::$sdk->request(__FUNCTION__, $param) ?: self::$sdk->debug;
+		$res = self::$sdk->request(__FUNCTION__, $param);
+		$this->result = self::$sdk->result;
+		return $res;
 	}
 
 
@@ -31,7 +31,8 @@ class TWUsa extends \Peak\SDK\TWUsa\Core {
 			'store_name' => $storeName,
 			'product_sn' => $productSn
 		]);
-		return $res ?: self::$sdk->debug;
-	}*/
+		$this->result = self::$sdk->result;
+		return $res;
+	}
 
 }
