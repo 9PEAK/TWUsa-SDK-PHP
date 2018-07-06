7b57c9a63eb9658aefb3b4b046cbe9a7b118a763
Kane <52653784@qq.com>
2018-07-06 16:24:29 +0800

    增加 运费查询接口

--- src/SDK/TWUsa/Module/Express.php
+++ src/SDK/TWUsa/Module/Express.php
@@ -6,23 +6,23 @@ trait Express {
 
 
 	/**
-	 * get the product inventory detail
+	 * estimate freight
 	 * */
-	protected static $estimateFreight = 'agent/v1/api/logistics/index';
+	protected static $estimateFreight = 'agent/v1/api/express/query';
 	protected function estimateFreight (array &$param)
 	{
 		return @ [
 			'store_name' => $param['store_name'],
-			'carrier' => $param['carrier'],
-			'delivery' => $param['delivery'],
-			'package' => $param['package'],
+			'carrier' => $param['carrier'] ?: '',
+			'delivery' => $param['delivery'] ?: '',
+			'package' => $param['package'] ?: '',
 			'postcode' => $param['postcode'],
 			'weight' => $param['weight'],
 			'length' => $param['length'],
 			'width' => $param['width'],
 			'height' => $param['height'],
-			'country_code' => $param['country_code'],
-			'state_code' => $param['state_code'],
+			'country_code' => $param['country_code'] ?: '',
+			'state_code' => $param['state_code'] ?: '',
 		];
 	}
 
